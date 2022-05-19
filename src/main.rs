be5f3d40c37547470ee72c3331f8c54fab1f1b66
Jean-Marc Alliot <jm@alliot.org>
2022-05-19 21:02:03 +0200

    Faster

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-//const SIZE:usize = 1000000000;
-const SIZE:usize = 100;
+const SIZE:usize = 1000000000;
 
 fn build_primes () -> Vec<u32> {
     let mut p = Vec::new();
@@ -22,29 +21,44 @@ fn build_primes () -> Vec<u32> {
     return p;
 }
 
-fn do_it() {
+fn liouville()-> Box<[u8;SIZE]> {
     let mut tab = Box::new([0 as u8; SIZE]);
-    tab[1]=0;tab[2]=1;
-    let mut num = 1-1;
+    tab[1]=1;tab[2]=0;
+    let primes = build_primes();
+
+    let mut a = 3;
+    let mut b;
+    while a<SIZE {
+	b=2*a-1;
+	if b>=SIZE {b=SIZE-1}
+	let m = (b as f64+0.01).sqrt() as usize;
+	for p in primes.iter() {
+	    let p = *p as usize;
+	    if p>m {break}
+	    let mut k = a/p;
+	    let mut mp = k*p;
+	    if mp<a {mp=mp+p;k=k+1}
+	    loop {
+		if mp>b {break}
+		tab[mp]= 1^tab[k];
+		mp=mp+p;
+		k=k+1;
+	    }
+	}
+	a=b+1
+    }
+    return tab;
+}
+
+fn main() {
+    let tab = liouville();
     let (mut first,mut last)=(0,0);
     let (mut imax,mut maxi)=(0,i32::MIN);
     let (mut imin,mut mini)=(0,i32::MAX);
-    let p = build_primes();
+    let mut num = 0 as i32;
     for i in 3..SIZE {
-	let mut j = 0;
-	let st = (i as f64+0.01).sqrt() as u32;
-	loop  {
-	    if (i as u32) % p[j] == 0 || p[j] > st {break}
-	    j=j+1;
-	}
-	if p[j] > st {
-	    tab[i]=1;
-	    num=num-1}
-	else {
-	    tab[i]=1+tab[(i as u32/p[j]) as usize];
-	    if tab[i]%2 == 0 {num=num+1}
-	    else {num=num-1}
-	}
+	if tab[i]==0 {num=num-1}
+	else {num=num+1}
 	if num > 0 {last=i}
 	if num > 0 && first == 0 {first=i}
 	if num > maxi {maxi=num;imax=i}
@@ -52,35 +66,4 @@ fn do_it() {
     }
     println!("first={},imax={},maxi={},imin={},mini={},last={})",
 	     first,imax,maxi,imin,mini,last);
-}
-
-fn do_it2() {
-    let mut tab = Box::new([0 as u8; SIZE]);
-    tab[1]=1;tab[2]=0;
-    let primes = build_primes();
-    
-    for p in primes.iter() {
-	print!("p={}:",p);
-	let mut v = 0;
-	loop  {
-	    for _i in 0..p-1 {
-		v=v+(*p as usize);
-		if v>= SIZE {break}
-		tab[v] = tab[v]^1;
-		print!("{} ",v);
-	    }
-	    v=v+(*p as usize);
-	    if v>=SIZE {break}
-	}
-	tab[*p as usize]=0;
-	println!("");
-    }
-    println!("{:?}",tab);
-
-//    println!("first={},imax={},maxi={},imin={},mini={},last={})",
-//	     first,imax,maxi,imin,mini,last);
-}
-
-fn main() {
-    do_it2();
 }
